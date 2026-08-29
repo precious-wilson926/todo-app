5913710d0c5f78975f8cfbd49e39a9cfc129f084
Precious <precious@example.com>
2026-08-29 19:48:03 +0100

    Add server code files

---
+++ server/db.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+
+const pool = process.env.DATABASE_URL
+  ? new Pool({ connectionString: process.env.DATABASE_URL })
+  : new Pool({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+
+module.exports = pool;
